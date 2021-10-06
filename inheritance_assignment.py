d61128dc53bc539cc9873651b4f419e58f4f397e
alishawillman <72734094+alishawillman@users.noreply.github.com>
2021-10-06 05:48:14 -0600

    inheritance_assignment
    creating parent and child classes

---
+++ inheritance_assignment.py
@@ -0,0 +1,21 @@
+
+
+#parent class
+class Beverages:
+    name = ''
+    total_sugars = ''
+    calories = ''
+
+#child class
+class Energy_Drink(Beverages):
+    caffine_content = ''
+    other_stimulents = ''
+    added_sugars = ''
+
+#child class
+class Smoothies(Beverages):
+    fruits_added = ''
+    protein_powder = ''
+    vegetables_added = ''
+    
+    
